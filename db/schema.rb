93879e28387f703d1ff21bbde2c6aa8ecf9a50c1
Alec Reid <alecreid0093@gmail.com>
2019-12-30 14:04:38 -0600

    auth updates

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_12_11_182455) do
     t.string "name"
     t.string "description"
     t.string "creator"
+    t.string "title"
+    t.string "platform"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
